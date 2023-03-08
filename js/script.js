24f1886314c36d9797cbb6ac8a979fb2739a1bc2
marcodedemo <marco.dedemo@gmail.com>
2023-03-08 12:49:35 +0100

    feat: milestone 0

---
+++ js/script.js
@@ -0,0 +1,56 @@
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
+
+
+
+
+
+const members = [
+
+    {
+        fullName: "Wayne Barnett",
+        role: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        fullName: "Angela Caroll",
+        role: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        fullName: "Walter Gordon",
+        role: "Office Manager",
+        img: "walter-gordon-office-manager.jpg"
+    },
+    {
+        fullName: "Angela Lopez",
+        role: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        fullName: "Scott Estrada",
+        role: "Developer",
+        img: "scott-estrada-developer.jpg"
+    },
+    {
+        fullName: "Barbara Ramos",
+        role: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg"
+    },
+];
+
+
+console.log(members);
